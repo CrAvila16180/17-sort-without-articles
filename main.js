cb8a5d7e0f50c41021cc3c8716d3d2c1dc63b424
CR_Avila <carlosrobertoavilah@gmail.com>
2019-01-17 09:54:32 -0600

    Change bands

--- main.js
+++ main.js
@@ -1,8 +1,63 @@
 
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = [
+    'Alesana',
+    'All Time low',
+    'Ancud',
+    'Andy Black',
+    'Asking Alexandria',
+    'Avenged Sevenfold',
+    'Beyond Unbroken',
+    'Black Sabbath',
+    'Black Veil Brides',
+    'Bring Me The Horizon',
+    'Bruce Dickinson',
+    'Bullet For My Valentine',
+    'Danny Worsnop',
+    'Escape The Fate',
+    'Fall Out Boy',
+    'Falling In Reverse',
+    'Feeling Every Sunset',
+    'From First To Last',
+    'German Garmendia',
+    'Ghost B.C.',
+    'Green Day',
+    'Human Fortress',
+    'Iron Maiden',
+    'Jacky Vincent',
+    'John 5',
+    'Krewella',
+    'LEDGER',
+    'Lindsey Stirling',
+    'Linkin Park',
+    'Lostprophets',
+    'Mägo de Oz',
+    'Marilyn Manson',
+    'Metallica',
+    'Motionless In White',
+    'Mushroomhead',
+    'My chemical Romance',
+    'Myrath',
+    'Nirvana',
+    'Ozzy Osbourne',
+    'Panic! At The Disco',
+    'Paramore',
+    'Pierce The Veil',
+    'ROAM',
+    'SABATON',
+    'Simple Plan',
+    'Skillet',
+    'Sleeping With Sirens',
+    'Slipknot',
+    'System of a Down',
+    'Three Days Grace',
+    'Twenty One Pilots',
+    'Twilight Force',
+    'Upon This Dawning',
 
+
+]
 function strip(bandName){
-    return bandName.replace(/^(a|the|an)/i, '').trim();
+    return bandName.replace(/^(a |the|an )/i, '').trim();
 }
 
 const sortedBands = bands.sort((a,b) =>  (strip(a) > strip(b)) ? 1 : -1);
